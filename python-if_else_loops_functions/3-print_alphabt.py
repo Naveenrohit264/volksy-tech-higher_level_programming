080899c4a57659b18dc5838230c859578f1b4709
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-02 00:01:46 -0800

    hi

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
+for i in range(ord('a'), ord('z')+1):
+    if i == ord('e') or i == ord('q'):
         continue
-    print("{:c}".format(alpha_letters), end="")
+    print("{:c}".format(i), end="")
