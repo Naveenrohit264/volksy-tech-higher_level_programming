e1ff851a7ea2f2e1e1ab25b184a9e45d7a30c2d0
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-03 08:27:25 -0800

    h

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return(c)
-    if c > b:
-        return (a + b)
-    else:
-        return ((a * b) - c)
+if __name__ == "__main__":
+    def magic_calculation(a, b, c):
+        if a < b:
+            return(c)
+        if c > b:
+            return (a + b)
+        else:
+            return ((a * b) - c)
