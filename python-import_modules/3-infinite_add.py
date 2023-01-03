25e936de3ae5191f5d508d6b74a58596c9547b1b
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-03 01:13:44 -0800

    hi

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def sum_all(*args):
-    try:
-        return sum(int(i) for i in args)
-    except:
-        return False
+if __name__ == "__main__:
+    def sum_all(*args):
+        try:
+            return sum(int(i) for i in args)
+        except:
+            return False
 print(sum_all())
