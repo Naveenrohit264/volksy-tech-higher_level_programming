f72b8c41a9fe3f5163eacfb7b12a4e3c8985e7fe
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-04 21:05:13 -0800

    hi

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list):
+    if len(my_list) < 1:
+        return None
+    higher = 0
+    for i in my_list:
+        if i > higher:
+            higher = i
+    return higher
