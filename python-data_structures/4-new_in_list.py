412d7a446cc99bfa1cebf3055b36aabfaa1c2fa2
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-05 02:01:46 -0800

    hi

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     x = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    x[idx]=element
+    x[idx] = element
     return x
