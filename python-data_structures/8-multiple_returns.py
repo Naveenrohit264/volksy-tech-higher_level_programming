dfde011416ee7bfbbb9684135dcfdde2cfe43b97
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-05 00:12:43 -0800

    hi

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence)==0:
-        return None
+        first= ''
     else:
-        first=(len(sentence),sentence[0])
+        print("Length: {:d} - First character: {}".format(length, first))
