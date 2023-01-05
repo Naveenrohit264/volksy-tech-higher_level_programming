fcab6493dd3a5afd70790b4f8ec39aa9618311f1
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-05 01:28:02 -0800

    hi

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None or sentence == o:
-        first = (0,None)
+    first = ()
+    if sentence is None or len(sentence) == 0:
+        first = (0, None)
     else:
-        first(len(sentence),sentence[0])
+        first = (len(sentence), sentence[0])
+    return first
