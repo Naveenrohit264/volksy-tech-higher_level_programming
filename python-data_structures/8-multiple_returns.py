8a351c26e1160a44112c34b01f69a2bfaf54a248
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-05 01:23:47 -0800

    hi

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-length= len(sentence)
-first= sentence[0]
 def multiple_returns(sentence):
-    if len(sentence)==0:
-        first= ''
+    if sentence is None or sentence == o:
+        first = (0,None)
     else:
-        print("Length: {:d} - First character: {}".format(length, first))
+        first(len(sentence),sentence[0])
