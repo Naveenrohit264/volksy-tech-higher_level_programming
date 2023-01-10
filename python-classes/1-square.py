60161de46b20291b9e4c171956e264da8fff997c
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-10 05:01:16 -0800

    hi

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """creating class for square"""
 
+
 class Square:
     """square class"""
 
-    def __init__(self, size):
+    def __init__(self,size):
         """constructor"""
         self.__size= size
