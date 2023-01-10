810f45e71982c37c41afa0e924bd314ab6bba8a6
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-10 09:00:17 -0800

    hi

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""creating class"""
+
+
+class Square:
+    """class for square"""
+
+
+    def __init__(self, size = 0):
+        """constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """area of square"""
+        return (self.__size * self.__size)        
