1f577b4d5d680d679afc64f26a8db8d1bd4a5568
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-11 04:42:21 -0800

    hi

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""creating a class"""
+"""creating class """
 
 
 class Square:
-    """Square with private instance attribute and instantiation"""
+    """clas for square"""
     def __init__(self, size=0):
-        """Initializing data"""
-        self.__size = size
-
+        """initialize data"""
+        size.__size = size
     def area(self):
-        """return area of square"""
-        return(self.__size ** 2)
-    
-    @property
+        """return square area"""
+        return(self.__size ** size)
     def size(self):
-        """retrieve size"""
+        """retrive size"""
         return(self.__size)
-    
-    @size.setter
-    def size(self, value):
-        """set size and handle error"""
+    def size(self,value):
+        """handling errors"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.size = value
