0fbd66f2eb630fcd4cd8cb973e22eb39d6bf5de1
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-11 04:06:02 -0800

    hi

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,7 +11,7 @@ class Square:
 
     def area(self):
         """return area of square"""
-    return(self.__size * self.__size)
+    return(self.__size ** 2)
     
     @property
     def size(self):
@@ -22,7 +22,7 @@ class Square:
     def size(self, value):
         """set size and handle error"""
         if type(value) is not int:
-            return TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
