fbe318500407d4275fa655f068410607ee2a0223
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-12 23:21:10 -0800

    hi

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defining function to check whether 
+   the type of object is class or not"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if type of objects is class
+    else return false"""
+    if type(obj) is class:
+        return True
+    else:
+        False
