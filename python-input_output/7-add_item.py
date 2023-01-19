5661a2ee4f2cbb55460f010f106de55c95edab95
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-18 21:06:09 -0800

    hi

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script"""
+import sys
+save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
+
+name = "add_item.json"
+load = []
+
+try:
+    load = load_from_json_file(name)
+except:
+    pass
+
+for i in range(1, len(sys.argv)):
+    load.append(sys.argv[i])
+save_to_json_file(load, name)
