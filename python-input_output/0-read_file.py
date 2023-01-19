1822a1b093a522d8a0039880ed9b00b7bb13ceaf
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-18 20:46:38 -0800

    hi

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Read file"""
+
+
+def read_file(filename=""):
+    """ read a file"""
+    if filename:
+        with open(filename, mode='r', encoding="utf-8") as f:
+            for line in f:
+                print(line, end="")
