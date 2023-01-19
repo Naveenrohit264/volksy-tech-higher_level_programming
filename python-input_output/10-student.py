a574a7b3a2b0d1a8ff7230a821bc9cccfbcb0681
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-18 21:17:57 -0800

    hi

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     """Student Class"""
-    def _init_(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initializer method"""
         self.first_name = first_name
         self.last_name = last_name
