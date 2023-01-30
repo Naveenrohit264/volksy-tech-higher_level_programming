cd6eedb606e5da5f2758e3cef7cb3560e1973243
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-30 01:40:53 -0800

    hi

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- grouping
-SELECT score, COUNT(*) as NUM FROM second_table GROUP BY name, ORDER BY score DESC;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY name, ORDER BY number DESC;
