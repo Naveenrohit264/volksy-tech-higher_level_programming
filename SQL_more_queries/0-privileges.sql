e314da4c97055e32357645b5e155f3fbbbc52ffa
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-01-31 08:23:40 -0800

    hi

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- listing all privileges
-SELECT * FROM FN_MY_PERMISSIONS(NULL, 'user_0d_1');
-SELECT * FROM FN_MY_PERMISSIONS(NULL, 'user_0d_2');
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
