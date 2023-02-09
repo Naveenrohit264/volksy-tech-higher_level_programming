b12ca1bdb42461ee68d7327ac8bfaa518c07bd0d
Naveenrohit264 <naveenp@brightcomgroup.com>
2023-02-08 21:16:57 -0800

    hi

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Asc order using joins"""
+"""Connection for python and sql"""
 
 
 import sys
@@ -8,10 +8,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM cities \
-               INNER JOIN states \
-               ON cities.state_id = states.id \
-               ORDER BY cities.id")
-    for i in c:
-        if i[4] == sys.argv[4]:
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    s = c.fetchall()
+    for i in s:
+        if i[1][0] == 'N':
             print(i)
